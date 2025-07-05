bef83e9d225ae3b8ed1d24a6919776354dcca25f
Sogyal <sogyal.v@gmail.com>
2025-07-05 19:46:07 +0530

    Final commit of The Garden URP WebGL build, with LFS-tracked assets

---
+++ Build/TheGardenWwise.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:829e07316bc07619d4c5abdeef723c4c27986fe835dfc85905e6a23ed313d70d
+size 475279
---
+++ StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUSH = 1427625975U;
+        static const AkUniqueID CHIMES = 2590078866U;
+        static const AkUniqueID CRICKETS = 1443984213U;
+        static const AkUniqueID RIVER_FLOW = 1007505908U;
+        static const AkUniqueID WATER = 2654748154U;
+        static const AkUniqueID WIND = 1537061107U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DOOR_PROXIMITY = 2385747627U;
+        static const AkUniqueID EMITTER_VOLUME = 3228788358U;
+        static const AkUniqueID FREQ_DISTANCE_ATTENUATION = 2923642054U;
+        static const AkUniqueID HEAD_ORIENTATION = 3257439120U;
+        static const AkUniqueID WIND_INTENSITY = 855296609U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ENVIRONMENT_BUS = 2833159698U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
